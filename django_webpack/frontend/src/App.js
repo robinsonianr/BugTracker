ee1f07f4ed5c68f540fdd77501a7b38f2fb71b57
Ian Robinson <ianrrob2@aol.com>
2022-05-23 17:59:01 -0400

    Initial django commit

---
+++ django_webpack/frontend/src/App.js
@@ -0,0 +1,13 @@
+import reactlogo from './reactlogo.png';
+import djangologo from './djangologo.png';
+import './App.css';
+
+function App() {
+	return (
+		<div>
+			<p> Hello App!</p>
+		</div>
+	);
+}
+
+export default App;
