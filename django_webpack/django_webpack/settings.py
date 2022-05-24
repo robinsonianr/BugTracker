56240872a1d08dc725b11a7237a4af824e0e599d
Ian Robinson <ianrrob2@aol.com>
2022-05-24 13:42:19 -0400

    updated issue settings/urls/views

--- django_webpack/django_webpack/settings.py
+++ django_webpack/django_webpack/settings.py
@@ -132,7 +132,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 
 REST_FRAMEWORK = {
-    'DEFAULT_RENDERER_CLASSES': (
-        'rest_framework.renderers.JSONRenderer',
-    )
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
 }
--- django_webpack/issues/urls.py
+++ django_webpack/issues/urls.py
@@ -1,6 +1,13 @@
-from django.urls import path
+from xml.etree.ElementInclude import include
+from django.urls import path, include
+from rest_framework import routers
 from . import views
 
+router = routers.DefaultRouter()
+router.register(r'user' ,views.IssueListCreate)
+
 urlpatterns = [
-    path('api/issue/', views.IssueListCreate.as_view()),
+    #path('api/issue/', views.IssueListCreate.as_view()),
+    path('', include(router.urls)),
+    path('api-auth/', include('rest_framework.urls', namespace='rest_framework'))
 ]
