4d19ac0ba34b38d6a93807ff80a5b36eba6194ff
Ian Robinson <ianrrob2@aol.com>
2022-05-24 13:12:44 -0400

    issue model added

--- django_webpack/django_webpack/settings.py
+++ django_webpack/django_webpack/settings.py
@@ -39,7 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_webpack_dev_server',
     'MySQLdb',
-    "frontend"
+    "frontend",
+    'issues.apps.IssuesConfig',
+    'rest_framework',
 ]
 
 MIDDLEWARE = [
@@ -127,3 +129,10 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+REST_FRAMEWORK = {
+    'DEFAULT_RENDERER_CLASSES': (
+        'rest_framework.renderers.JSONRenderer',
+    )
+}
--- django_webpack/issues/views.py
+++ django_webpack/issues/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
+from .models import Issue
+from .serializers import IssueSerializer
+from rest_framework import generics
 
 # Create your views here.
+
+class IssueListCreate(generics.ListCreateAPIView):
+    queryset = Issue.objects.all()
+    serializer_class = IssueSerializer
