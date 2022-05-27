69933f4089c581f9dc65f691c1c1436d036c95b5
Ian Robinson <ianrrob2@aol.com>
2022-05-26 20:09:05 -0400

    added material ui

---
+++ django_webpack/frontend/src/components/Dashboard.js
@@ -0,0 +1,13 @@
+import React from "react";
+import Typograpghy from "@material-ui/core/Typography";
+
+export default function Dashboard() {
+  return (
+    <div>
+      {/* <Typograpghy variant="h1" color="primary" align="center">
+        View Dashboard
+      </Typograpghy> */}
+      <Typograpghy color="secondary">Lorem, ip sum do lorem</Typograpghy>
+    </div>
+  );
+}
