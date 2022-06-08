489698cc94bc22acced0dfb51f21a05c8bd6f4fa
Ian Robinson <ianrrob2@aol.com>
2022-06-08 00:58:35 -0400

    database fetch

--- django_webpack/frontend/src/components/datatable/DataTable.jsx
+++ django_webpack/frontend/src/components/datatable/DataTable.jsx
@@ -3,9 +3,9 @@ import "./datatable.scss"
 import { DataGrid } from '@mui/x-data-grid';
 
 const columns = [
-  { field: 'id', headerName: 'ID', width: 70 },
-  { field: 'title', headerName: 'Title', width: 130 },
-  { field: 'description', headerName: 'Description', width: 130 },
+  { field: 'id', headerName: 'ID', width: 30 },
+  { field: 'title', headerName: 'Title', width: 200 },
+  { field: 'description', headerName: 'Description', width: 500 },
   {
     field: 'priority',
     headerName: 'Priority',
@@ -15,29 +15,27 @@ const columns = [
   {
     field: 'issue_type',
     headerName: 'Issue Type',
-    width: 160,
+    width: 100,
+    // valueGetter: (params) => `${params.columns.issue_type}`
   },
-  { field: 'date_added', headerName: 'Dated added', width: 130 },
+  { field: 'date_added', headerName: 'Dated added', width: 220 },
   { field: 'created_by', headerName: 'Created By', width: 130 }
 ];
 
-const rows = [
-  { id: 1, lastName: 'Snow', firstName: 'Jon', priority: 35 },
-  { id: 2, lastName: 'Lannister', firstName: 'Cersei', age: 42 },
-  { id: 3, lastName: 'Lannister', firstName: 'Jaime', age: 45 },
-  { id: 4, lastName: 'Stark', firstName: 'Arya', age: 16 },
-  { id: 5, lastName: 'Targaryen', firstName: 'Daenerys', age: null },
-  { id: 6, lastName: 'Melisandre', firstName: null, age: 150 },
-  { id: 7, lastName: 'Clifford', firstName: 'Ferrara', age: 44 },
-  { id: 8, lastName: 'Frances', firstName: 'Rossini', age: 36 },
-  { id: 9, lastName: 'Roxie', firstName: 'Harvey', age: 65 },
-];
 
 export default function DataTable() {
+
+    const [tableData, setTableData] = useState([])
+
+    useEffect(() => {
+         fetch("http://localhost:8080/issues/api/issue")
+        .then((data) => data.json())
+        .then((data) => setTableData(data)) 
+    }, [])
   return (
     <div className="datatable">
          <DataGrid
-        rows={rows}
+        rows={tableData}
         columns={columns}
         pageSize={5}
         rowsPerPageOptions={[5]}
