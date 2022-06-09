a1f89d664e509d2777da441da9bdf5ec0232ecc3
Ian Robinson <ianrrob2@aol.com>
2022-06-08 22:56:11 -0400

    form setup

--- django_webpack/frontend/src/components/pages/createIssues/CreateIssue.jsx
+++ django_webpack/frontend/src/components/pages/createIssues/CreateIssue.jsx
@@ -10,6 +10,29 @@ export default function CreateIssue() {
       <Sidebar />
       <div className="createContainer">
         <Navbar/>
+        <div className="createForm">
+          <div className="title">
+            <label>Title</label>
+            <input type="text"  />
+          </div>
+          <div className="description">
+            <label>Description</label>
+            <textarea type="text"  className="inp" />
+          </div>
+          <div className="priority">
+            <label>Priority</label>
+            <input type="text" />
+          </div>
+          <div className="issueType">
+            <label>Issue Type</label>
+            <input type="text" />
+          </div>
+          <div className="createdBy">
+            <label>Created By</label>
+            <input type="text" />
+          </div>
+          
+        </div>
       </div>
     </div>
   );
